ee9e1ba6a1b8b4d274ccc2deec328de63c38eca3
Jill Kuchman <pillanoid@gmail.com>
2015-10-07 13:02:50 -0700

    Add dictionary to /rectangle route

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,15 +12,22 @@ namespace RectangleChecker
         return View["index.cshtml"];
       };
       Get["/rectangle"] = _ => {
-        dynamic model = new DynamicDictionary();
+        // dynamic model = new DynamicDictionary();
         // var myRectangle = new Rectangle (Request.Query["side-length"], Request.Query["side-width"]);
+        Dictionary<string, object> shapes = new Dictionary<string, object>();
         var myRectangle = new Rectangle {
           Length = Request.Query["side-length"],
           Width = Request.Query["side-width"]
         };
-        model.Rectangle = myRectangle;
+        // shapes.Rectangle = myRectangle;
+        var rectangle2 = new Rectangle {
+          Length = 100,
+          Width = 5
+        };
+        shapes.Add("shape1", myRectangle);
+        shapes.Add("shape2", rectangle2);
 
-        return View["rectangle.cshtml", model];
+        return View["rectangle.cshtml", shapes];
       };
     }
   }
