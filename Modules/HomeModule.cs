bda38e66dbbb6bffdae614be403912e4d8566cca
Jill Kuchman <pillanoid@gmail.com>
2015-10-07 13:07:19 -0700

    Add private/public with constructor and getter/setter methods

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,20 +12,14 @@ namespace RectangleChecker
         return View["index.cshtml"];
       };
       Get["/rectangle"] = _ => {
-        // dynamic model = new DynamicDictionary();
-        // var myRectangle = new Rectangle (Request.Query["side-length"], Request.Query["side-width"]);
         Dictionary<string, object> shapes = new Dictionary<string, object>();
-        var myRectangle = new Rectangle {
-          Length = Request.Query["side-length"],
-          Width = Request.Query["side-width"]
-        };
-        // shapes.Rectangle = myRectangle;
-        var rectangle2 = new Rectangle {
-          Length = 100,
-          Width = 5
-        };
+
+        // var myRectangle = new Rectangle {
+        //   Length = Request.Query["side-length"],
+        //   Width = Request.Query["side-width"]
+        // };
+        Rectangle myRectangle = new Rectangle(Request.Query["side-length"], Request.Query["side-width"]);
         shapes.Add("shape1", myRectangle);
-        shapes.Add("shape2", rectangle2);
 
         return View["rectangle.cshtml", shapes];
       };
